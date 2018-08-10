8f24b3bdb19d4f8da523d1c7bac10acf65b091d8
Alec <afroenygam@gmail.com>
2018-08-10 13:02:55 -0400

    Backend/MVP completed

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('./middlewares');
 
@@ -10,10 +13,42 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+
+  if (user.username && user.password) {
+    db.insert(user).into('users').then(response => {
+      db('users').where('id', response[0]).first().then(result => {
+        res.status(201).json(result)
+      }).catch(err => {
+        console.log('inner error', err);
+      });
+    }).catch(err => {
+      console.log('outer error', err);
+      res.status(500).json({message:`${err}`});
+    })
+  }
 }
 
-function login(req, res) {
+let token = jwt.sign({
+  something:"who cares"
+}, 'Why can’t banks keep secrets? There are too many tellers!');
+
+async function login(req, res) {
   // implement user login
+  try {
+    const credentials = req.body;
+    const foundUser = await db('users').where('username', credentials.username).first();
+    const userHash = foundUser.password;
+    let isValid = bcrypt.compareSync(credentials.password, userHash);
+    if (isValid) {
+      res.status(200).json({message:"Logged In", user:credentials.username, token:token});
+    }
+  } catch (err) {
+    res.status(401).json({message:`${err}`});
+  }
+
 }
 
 function getJokes(req, res) {
